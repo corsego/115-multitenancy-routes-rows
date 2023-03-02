ad830a9f3b0fdcfed699191c0132cdaf21691e29
yshmarov <yshmarov@gmail.com>
2023-03-03 00:20:26 +0100

    invite users to be members of tenant

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,6 +5,18 @@ class MembersController < ApplicationController
     @member = @current_tenant.members
   end
 
+  def invite
+    email = params[:email]
+    return redirect_to tenant_members_path(@current_tenant), alert: 'No email provided' if email.blank?
+
+    user = User.find_by(email:) || User.invite!({ email: }, current_user)
+    return redirect_to tenant_members_path(@current_tenant), alert: 'Email invalid' unless user.valid?
+
+    user.members.find_or_create_by(tenant: @current_tenant, roles: {admin: false, editor: true})
+    # binding.b
+    redirect_to tenant_members_path(@current_tenant), notice: "#{email} invited!"
+  end
+
   private
 
   def set_current_tenant
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :tenants do
-    resources :members
+    resources :members do
+      collection do
+        post :invite
+      end
+    end
   end
   devise_for :users
   root 'static_pages#landing_page'
